65ef1ce1598af17713a5116497151b9c3a630e04
Kasieswar <kasieswar26@gmail.com>
2024-12-31 19:10:00 +0530

    explore

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 function Navbarr() {
   return (
@@ -10,8 +11,8 @@ function Navbarr() {
             <Navbar.Toggle aria-controls="navbar" />
             <Navbar.Collapse id="navbar">
             <Nav className="ms-auto">
-                <Nav.Link href="/travel-journal">Home</Nav.Link>
-                <Nav.Link href="/explore">Explore Map</Nav.Link>
+                <Link className='link' to={"/travel-journal"}>Home</Link>
+                <Link className='link' to={"/explore"}>Explore Map</Link>
             </Nav>
             </Navbar.Collapse>
             </Container>
